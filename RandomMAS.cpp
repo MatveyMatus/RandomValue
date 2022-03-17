6805e85574729a422289f32cd878b7f6f311619b
MatveyMatus <91337034+MatveyMatus@users.noreply.github.com>
2022-03-17 19:47:21 +0500

    Add files via upload

---
+++ RandomMAS.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <ctime>
+using namespace std;
+
+// вывод массива в консоль
+void print(int (&arr)[10000]) {
+    for (int i = 0; i < 10000; ++i) {
+        if (i % 20 == 0) {
+            cout << endl;
+        }
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+// нахождение среднего значения в массиве
+void meanValue(int(&arr)[10000]) {
+    int sum = 0;
+    for (int i = 0; i < 10000; ++i) {
+        sum += arr[i];
+    }
+    cout << endl << "meanValue: " << (sum * 1.0) / (10000 * 1.0);
+}
+
+int main()
+{
+    const int SIZE = 10000;
+    int Array[SIZE]{};
+    srand(time(NULL));
+
+    for (int i = 0; i < SIZE; ++i) {
+        Array[i] = rand() % 201 - 100;
+    }
+
+    print(Array);
+    meanValue(Array);
+}
